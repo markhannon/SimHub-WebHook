54609eaaa90a45bbf2009699af6e8397a8250f02
Mark Hannon <mark.hannon@gmail.com>
2026-03-16 12:33:11 +1100

    Add batch file and install task

---
+++ Install-Files.ps1
@@ -0,0 +1,32 @@
+# 
+# Install latests files to simhub
+#
+
+param (
+    [switch]$dashboards = $false,
+    [switch]$overlays = $false
+)
+
+$SettingsObject = Get-Content -Path settings.json | ConvertFrom-Json
+
+Set-PSDebug -Trace 0
+
+$sections = "batch", "json", "powershell"
+foreach ($section in $sections) {
+    Write-Host "Installing $section files..."
+    $collection = $SettingsObject.$section
+    Write-Host "Found $($collection.Count) items in $section section."
+    foreach ($item in $collection) {
+        $src = $SettingsObject.src
+        $dst = $SettingsObject.dst
+        $fileName = $item.name
+        robocopy `
+            $src\ `
+            $dst\ `
+            "$fileName" `
+            /xd ".git" `
+            /xd ".gitignore" `
+            /xd ".vscode" `
+            /mir
+    }
+}
