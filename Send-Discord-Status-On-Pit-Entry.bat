3f1272a3b012b9decedb51760ecb0a33f8d330d9
Mark Hannon <mark.hannon@gmail.com>
2026-03-16 21:27:26 +1100

    Removed uneeded bat files Create lnk files to install in SimHub for hidden execution

--- Send-Discord-Status-On-Pit-Entry.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%Send-Discord-Status.ps1
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%PowerShellScriptPath%" -Extra "Entering Pits"
-
